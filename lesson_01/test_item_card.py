8b04d4b742af12be3230b12107143bc34f9a261d
den_kho <20597299+denkho@users.noreply.github.com>
2023-10-15 18:09:50 +0300

    test of go from inventory to item card through item image click

--- lesson_01/test_item_card.py
+++ lesson_01/test_item_card.py
@@ -6,6 +6,7 @@ driver = webdriver.Chrome()
 
 URL = "https://www.saucedemo.com/"
 URL_ITEM = "https://www.saucedemo.com/inventory-item.html?id=4"
+URL_INVENTORY = "https://www.saucedemo.com/inventory.html"
 USER_NAME = "standard_user"
 PASSWD = "secret_sauce"
 
@@ -17,12 +18,23 @@ def login():
     driver.find_element(By.XPATH, '//input[@data-test="password"]').send_keys(PASSWD)
     driver.find_element(By.XPATH, '//input[@data-test="login-button"]').click()
 
-def test_get_item_from_inventory_by_title():
+def test_go_to_item_from_inventory_by_item_title():
     login()
 
     item_name = driver.find_element(By.XPATH, '//div[@class="inventory_item_name"]').text
     driver.find_element(By.XPATH, '//div[@class="inventory_item_name"]').click()
+    time.sleep(3)
     assert URL_ITEM in driver.current_url
     assert driver.find_element(By.XPATH, '//div[@class="inventory_details_name large_size"]').text == item_name 
 
+
+def test_go_to_item_from_inventory_by_item_image():
+
+    driver.get(URL_INVENTORY)
+    time.sleep(3)
+    item_name = driver.find_element(By.XPATH, '//div[@class="inventory_item_name"]').text
+    driver.find_element(By.XPATH, '//img[@class="inventory_item_img"]').click()
+    print(item_name)
+    assert URL_ITEM in driver.current_url
+    assert driver.find_element(By.XPATH, '//div[@class="inventory_details_name large_size"]').text == item_name 
     driver.close()
