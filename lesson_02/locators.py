8f8c1fc385cb4a7f8a8f67ee9f07675aeed627b9
den_kho <20597299+denkho@users.noreply.github.com>
2023-10-28 09:13:32 +0300

    test of filters lo-hi and hi-lo for prices

--- lesson_02/locators.py
+++ lesson_02/locators.py
@@ -20,6 +20,7 @@ ERROR_MESSAGE_LOGIN_FORM = (By.XPATH, '//h3[@data-test="error"]')
 # INVENTORY
 
 INVENTORY_ITEM = (By.XPATH, '//div[@class="inventory_item_name"]')
+INVENTORY_ITEM_PRICE = (By.XPATH, '//div[@class="inventory_item_price"]')
 
 
 # FILTERS
@@ -27,3 +28,5 @@ INVENTORY_ITEM = (By.XPATH, '//div[@class="inventory_item_name"]')
 SORT_CONTAINER = (By.XPATH, '//select[@data-test="product_sort_container"]')
 ZA_FILTER = (By.XPATH, '//option[@value="za"]')
 AZ_FILTER = (By.XPATH, '//option[@value="az"]')
+LOHI_FILTER = (By.XPATH, '//option[@value="lohi"]')
+HILO_FILTER = (By.XPATH, '//option[@value="hilo"]')
--- lesson_02/test_filters.py
+++ lesson_02/test_filters.py
@@ -22,7 +22,8 @@ def test_inventory_AZ_filter(driver):
     driver.find_element(*locators.SORT_CONTAINER).click()
     driver.find_element(*locators.ZA_FILTER).click()
     driver.find_element(*locators.AZ_FILTER).click()
-    
+    driver.implicitly_wait(3)
+
     # после фильтрования собираем перечень названий элементов в список
     items = driver.find_elements(*locators.INVENTORY_ITEM)
     items_lst_filtered = [item.text for item in items]
@@ -33,16 +34,59 @@ def test_inventory_AZ_filter(driver):
 def test_inventory_ZA_filter(driver):
     """Тест фильтра ZA"""
     login(driver)
+
     # собираем перечень названий элементов в список для последующей проверки фильтром
     items = driver.find_elements(*locators.INVENTORY_ITEM)
     items_lst_start = [item.text for item in items]
     
     driver.find_element(*locators.SORT_CONTAINER).click()
     driver.find_element(*locators.ZA_FILTER).click()
-    
+    driver.implicitly_wait(3)
+
     # после фильтрования собираем перечень названий элементов в список
     items = driver.find_elements(*locators.INVENTORY_ITEM)
     items_lst_filtered = [item.text for item in items]
 
     assert sorted(items_lst_start, reverse=True) == items_lst_filtered
 
+def test_inventory_price_low_high_sort_filter(driver):
+    """Тест фильтра цен от низкой к высокой"""
+    login(driver)
+
+    # собираем список цен элементов
+    prices = driver.find_elements(*locators.INVENTORY_ITEM_PRICE)
+    prices_lst_start = [float(p.text[1:]) for p in prices]
+
+    # включаем фильтр по ценам с меньшей к большей
+    driver.find_element(*locators.SORT_CONTAINER).click()
+    driver.find_element(*locators.LOHI_FILTER).click()
+    driver.implicitly_wait(3)
+
+    # собираем список цен элементов после фильтрования
+    prices = driver.find_elements(*locators.INVENTORY_ITEM_PRICE)
+    prices_lst_filtered = [float(p.text[1:]) for p in prices]
+
+    # сравниваем, что получилось
+    assert sorted(prices_lst_start) == prices_lst_filtered
+
+
+def test_inventory_price_high_low_sort_filter(driver):
+    """Тест фильтра цен от высокой к низкой"""
+    login(driver)
+
+    # собираем список цен элементов
+    prices = driver.find_elements(*locators.INVENTORY_ITEM_PRICE)
+    prices_lst_start = [float(p.text[1:]) for p in prices]
+
+    # включаем фильтр по ценам от большей к меньшей
+    driver.find_element(*locators.SORT_CONTAINER).click()
+    driver.find_element(*locators.HILO_FILTER).click()
+    driver.implicitly_wait(3)
+
+    # собираем список цен элементов после фильтрования
+    prices = driver.find_elements(*locators.INVENTORY_ITEM_PRICE)
+    prices_lst_filtered = [float(p.text[1:]) for p in prices]
+
+    # сравниваем, что получилось
+    assert sorted(prices_lst_start, reverse=True) == prices_lst_filtered
+    
